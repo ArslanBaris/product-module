ae4575f8c8e083a1658bb005866a55582f55c380
ArslanBaris <arslan.bariss@outlook.com>
2023-11-17 22:31:15 +0300

    UI Library added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import '@/styles/navbar.css'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+// import "/node_modules/primeflex/primeflex.css"
+import 'primeicons/primeicons.css';
+import "primeflex/themes/primeone-light.css"   
+import Topnav from '@/components/Topnav/Topnav';
+import { PrimeReactProvider } from 'primereact/api';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <PrimeReactProvider>
+        <div className='pr-4 pl-4'>
+          <Topnav />
+          <Component {...pageProps} />
+          </div>
+      </PrimeReactProvider>
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,1000&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://unpkg.com/primeflex@latest/primeflex.css"/>   
+       
+      </Head>
       <body>
         <Main />
         <NextScript />
