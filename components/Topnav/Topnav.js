190211b7e4510081968ef1acad132b3df6b86012
ArslanBaris <arslan.bariss@outlook.com>
2023-11-17 22:31:59 +0300

    Topnav created

---
+++ components/Topnav/Topnav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Topnav(props) {
+
+
+    return (
+        <>
+            <div className="navbar flex flex-row justify-content-center">
+                <div className="flex justify-content-center">
+                    <span className="navbar-text">Product Module</span>
+                </div>
+            </div>
+        </>
+    );
+}
